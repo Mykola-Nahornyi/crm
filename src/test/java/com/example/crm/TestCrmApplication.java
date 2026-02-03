47cfacde3c8a3054cfdda088fc4c455d0da35168
Mykola <colinpr07@email.com>
2026-02-03 23:19:09 +0100

    Environment setup

---
+++ src/test/java/com/example/crm/TestCrmApplication.java
@@ -0,0 +1,11 @@
+package com.example.crm;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CrmApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
